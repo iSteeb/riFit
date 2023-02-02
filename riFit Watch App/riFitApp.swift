c1bc37d75390df880e6335198a1b6e78031bbe0f
Steven Duzevich <steven@duz.ie>
2023-02-02 12:40:52 +0100

    Initial Commit

---
+++ riFit Watch App/riFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  riFitApp.swift
+//  riFit Watch App
+//
+//  Created by Steven Duzevich on 2/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct riFit_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
