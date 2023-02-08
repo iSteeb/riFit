90e834a49fdcbc83b092eefdfc31d0584a14ed4d
Steven Duzevich <steven@duz.ie>
2023-02-08 10:52:32 +0100

    better enums and set class

---
+++ riFit Watch App/fiveThreeOneEnum.swift
@@ -0,0 +1,15 @@
+//
+//  fiveThreeOneEnum.swift
+//  riFit Watch App
+//
+//  Created by Steven Duzevich on 8/2/2023.
+//
+
+import Foundation
+
+enum fiveThreeOneProgramType {
+    case five
+    case three
+    case one
+    case deload
+}
