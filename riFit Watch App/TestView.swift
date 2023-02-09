c27f7e573ac0fc1739b67fe5674bfc9e8c38478e
Steven Duzevich <steven@duz.ie>
2023-02-09 04:01:11 +0100

    testing cremation and spritekit

--- riFit Watch App/TestView.swift
+++ riFit Watch App/TestView.swift
@@ -6,8 +6,39 @@
 //
 
 import SwiftUI
+import SpriteKit
 import CoreMotion
 
+
+class GameScene: SKScene {
+    private let hamtaroSpriteNode = SKSpriteNode(imageNamed: "hamtaro")
+    private let motionManager = CMMotionManager()
+
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func sceneDidLoad() {
+        motionManager.startAccelerometerUpdates()
+        
+        // game scene
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+        // hamtaro node
+        hamtaroSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
+        hamtaroSpriteNode.size = CGSize(width: 20, height: 20)
+        self.addChild(hamtaroSpriteNode)
+        
+        hamtaroSpriteNode.physicsBody = SKPhysicsBody(circleOfRadius: hamtaroSpriteNode.size.width / 2)
+        hamtaroSpriteNode.physicsBody?.allowsRotation = true
+        hamtaroSpriteNode.physicsBody?.restitution = 0.5
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if let accelerometerData = motionManager.accelerometerData {
+            physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.x * 0.01, dy: accelerometerData.acceleration.y * 0.01)
+        }
+    }
+}
 struct ExerciseModalView: View {
     @Binding var isPresented: Bool
     
@@ -35,21 +66,23 @@ struct TestView: View {
     @State private var isPresentingModalView = false
     
     @State var numberOfPeople = 100
-
+    
+    var scene: SKScene {
+        let scene = GameScene(size: CGSize(width: 100, height: 100))
+        scene.scaleMode = .fill
+        
+        return scene
+    }
     
     var body: some View {
         VStack {
-            Button("Add Exercise") {
-                isPresentingModalView.toggle()
-            }
-            .fullScreenCover(isPresented: $isPresentingModalView) {
-                ExerciseModalView(isPresented: $isPresentingModalView)
-            }
-            Picker("Number of people", selection: $numberOfPeople) {
-                    ForEach(2 ..< 100) {
-                        Text("\($0) people")
-                    }
-            }
+            SpriteView(scene: scene)
+//            Button("Add Exercise") {
+//                isPresentingModalView.toggle()
+//            }
+//            .fullScreenCover(isPresented: $isPresentingModalView) {
+//                ExerciseModalView(isPresented: $isPresentingModalView)
+//            }
         }
     }
 }
